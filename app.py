1cbd3884f7daf9c3df62bb77922937b8bbe9cbf9
Alexagz79190 <mathon.alexis@agrizone.net>
2025-10-23 19:12:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="📦 Envoi états de commande", page_icon="📤", layout="wide")
+st.set_page_config(
+    page_title="📦 Envoi états de commande",
+    page_icon="📤",
+    layout="wide"
+)
 
-pg_envoi_cmd = st.Page("pages/1_📦 Envoi états de commande.py", title="📦 Envoi états de commande")
-pg_facture   = st.Page("pages/2_📑 Envoi facture.py",        title="📑 Envoi facture")
-
-nav = st.navigation([pg_envoi_cmd, pg_facture])
-nav.run()
-
+st.title("📦 Envoi états de commande")
+st.write("Bienvenue dans la page principale.")
